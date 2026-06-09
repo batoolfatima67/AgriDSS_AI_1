05d072712a055200c65171f697261bcd00e8eda6
Batool Fatima <fatima67fgb@gmail.com>
2026-06-09 11:08:24 +0500

    Update weather_module.py

--- modules/weather_module.py
+++ modules/weather_module.py
@@ -4,15 +4,11 @@ import requests
 
 def get_weather(lat, lon):
 
-    api_key = st.secrets["2a0c3869482067519b6719f19118aca4"]
+    api_key = st.secrets["OPENWEATHER_API_KEY"]
 
     url = (
         f"https://api.openweathermap.org/data/2.5/weather"
         f"?lat={lat}&lon={lon}&appid={api_key}&units=metric"
     )
 
-    response = requests.get(url)
-
-    return response.json()
-    
-   
+    return requests.get(url).json()
