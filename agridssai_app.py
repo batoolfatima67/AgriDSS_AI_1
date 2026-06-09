40c2673b60a7716b4fd082a93937ebde3c103f10
Batool Fatima <fatima67fgb@gmail.com>
2026-06-09 13:24:08 +0500

    Update agridssai_app.py

--- agridssai_app.py
+++ agridssai_app.py
@@ -2,6 +2,7 @@ import streamlit as st
 
 from modules.input_module import render_input_module
 from modules.analysis_engine import run_full_analysis
+from modules.dashboard import render_dashboard
 from modules.report_generator import generate_report
 
 st.set_page_config(page_title="AgriDSS_AI", layout="wide")
@@ -10,10 +11,13 @@ st.sidebar.title("🌾 AgriDSS_AI")
 
 page = st.sidebar.radio(
     "Navigation",
-    ["Farm Input", "Run Analysis", "Report"]
+    ["Farm Input", "Run Analysis", "Dashboard", "Report"]
 )
 
-if page == "Farm Input":
+if page == "Dashboard":
+    render_dashboard()
+    
+elif page == "Farm Input":
     render_input_module()
 
 elif page == "Run Analysis":
