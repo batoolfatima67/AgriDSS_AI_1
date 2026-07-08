9bfa16f108505d1f8119c99e5f0a5e40204a489a
Batool Fatima <fatima67fgb@gmail.com>
2026-07-08 21:35:55 +0500

    Change paragraph text color to white

--- agridssai_app.py
+++ agridssai_app.py
@@ -40,7 +40,7 @@ with st.sidebar:
         🌾 AgriDSS AI
         </h1>
 
-        <p style='text-align:center;color:#60A5FA;'>
+        <p style='text-align:center;color:white;'>
         Agricultural Intelligence Platform
         </p>
         """,
